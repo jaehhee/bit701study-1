248be6e0d7273dfaa9dcc639343e543372ffda3c
moonhee <zipsy327@gmail.com>
2023-03-20 17:32:54 +0900

    jsp

---
+++ javawork/Java701project/src/study/day0320/Test.java
@@ -0,0 +1,19 @@
+package study.day0320;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String a="12a";
+		
+		int b=Integer.parseInt(a);
+		
+		
+	}	
+
+}
